b05dc86989cda8acd48cac803b5ee4b34acbd8dd
Baltazar  Parra <baltazarparra@outlook.com>
2025-01-25 14:03:04 -0300

    update node module

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const express = require("express");
-require("dotenv").config();
+// index.js
+import express from "express";
+import "dotenv/config";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Rota Única para Validação do Webhook
+// Rota única de validação
 app.get("/webhook", (req, res) => {
-  const verifyToken = process.env.VERIFY_TOKEN; // Token definido no .env
-  const token = req.query["hub.verify_token"];
-  const challenge = req.query["hub.challenge"];
+  const {
+    "hub.mode": mode,
+    "hub.verify_token": token,
+    "hub.challenge": challenge
+  } = req.query;
 
-  if (req.query["hub.mode"] === "subscribe" && token === verifyToken) {
-    console.log("[SUCESSO] Webhook validado!");
-    res.status(200).send(challenge);
-  } else {
-    console.log("[ERRO] Token inválido ou modo incorreto");
-    res.sendStatus(403);
+  if (mode === "subscribe" && token === process.env.VERIFY_TOKEN) {
+    return res.status(200).send(challenge);
   }
+
+  res.sendStatus(403);
 });
 
-// Inicia o Servidor
 app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}/webhook`);
+  console.log(`Servidor rodando: http://localhost:${PORT}`);
 });
