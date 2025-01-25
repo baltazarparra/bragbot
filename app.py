7d3f929d71a7d8a72e49c0fcee30426cf29fa99e
Baltazar  Parra <baltazarparra@outlook.com>
2025-01-25 15:29:49 -0300

    open the snake gate

--- app.py
+++ app.py
@@ -47,3 +47,6 @@ def webhook():
     except Exception as e:
         print(f"Erro ao processar mensagem: {e}")
         return "Erro interno", 500
+    
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
